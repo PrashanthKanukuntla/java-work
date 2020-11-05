f53b6f8b98543e5e9b5fffeee5d481a81cf44152
Prashanth Kanukuntla <Prashanth Kanukuntla@DESKTOP-2C66BHS>
2020-11-05 23:01:59 +0530

    Nested if-else condition

--- java-work/src/Helloworld.java
+++ java-work/src/Helloworld.java
@@ -1,10 +1,17 @@
-import java.util.Scanner;
-public class Helloworld
-{
-public static void main(String[] args)
-{
-	Scanner banana = new Scanner(System.in);
-
-	System.out.println(banana.nextLine());
-}
+public class Helloworld{
+	public static void main(String[] args) {
+		int boy=25;
+		int girl=24;
+		
+		if(boy>21) {
+			System.out.println("The boy is eligible for marriage");
+			if(girl>18) {
+				System.out.println("the girl is eligible for marriage");
+			}else {
+				System.out.println("the girl is not eligible for marriage");
+			}
+		}else {
+				System.out.println("the boy is not eligible for marriage");
+		}
+	}
 }
