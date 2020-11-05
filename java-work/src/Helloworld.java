7a97dded1bfd25ddd12cf30f271a401a4d819ab3
Prashanth Kanukuntla <Prashanth Kanukuntla@DESKTOP-2C66BHS>
2020-11-05 08:31:27 +0530

    String object

--- java-work/src/Helloworld.java
+++ java-work/src/Helloworld.java
@@ -1,23 +1,15 @@
 public class Helloworld
 {
-public static void main(String[] args)
+public static void main(String[] args) 
 {
-	//get the minimum and maximum values of the variables
-	System.out.println(Byte.MIN_VALUE);
-	System.out.println(Byte.MAX_VALUE+"\n");
+	char charValue =66;
+	System.out.println(charValue);
 	
-	System.out.println(Short.MIN_VALUE);
-	System.out.println(Short.MAX_VALUE+"\n");
+	int integerValueOf='A';
+	System.out.println(integerValueOf);
 	
-	System.out.println(Integer.MIN_VALUE);
-	System.out.println(Integer.MAX_VALUE+"\n");
-	
-	System.out.println(Long.MIN_VALUE);
-	System.out.println(Long.MAX_VALUE+"\n");
-	
-	System.out.println(Float.MIN_VALUE);
-	System.out.println(Double.MAX_VALUE);
-	
- }
+	String randomString ="this is random String.";
+	String another_String="I write it";
+	System.out.println(randomString+" "+another_String);
+}	
 }
-
