a647cf52b4a1b903fe1e53a678bea711eabfd56b
Prashanth Kanukuntla <Prashanth Kanukuntla@DESKTOP-2C66BHS>
2020-11-05 09:11:04 +0530

    toString() method

--- java-work/src/Helloworld.java
+++ java-work/src/Helloworld.java
@@ -2,14 +2,44 @@ public class Helloworld
 {
 public static void main(String[] args) 
 {
-	char charValue =66;
-	System.out.println(charValue);
+	Byte bigByte=101;
+	System.out.println(bigByte);
 	
-	int integerValueOf='A';
-	System.out.println(integerValueOf);
+	String bigbyte_String =Byte.toString(bigByte);
+	System.out.println(bigbyte_String+"\n");
 	
-	String randomString ="this is random String.";
-	String another_String="I write it";
-	System.out.println(randomString+" "+another_String);
+	short bigShort =202;
+	System.out.println(bigShort);
+	
+	String bigShort_String=Short.toString(bigShort);
+	System.out.println(bigShort_String+"\n");
+	
+	
+	int bigInt=2020;
+	System.out.println(bigInt);
+	
+	String intString=Integer.toString(bigInt);
+	System.out.println(intString+"\n");
+	
+	
+	long bigLong=202020;
+	System.out.println(bigLong);
+	
+	String longString=Long.toString(bigLong);
+	System.out.println(longString+"\n");
+	
+	
+	float bigFloat =2343;
+	System.out.println(bigFloat);
+	
+	String bigFloat_String=Float.toString(bigFloat);
+	System.out.println(bigFloat_String+"\n");
+	
+	
+	double bigDouble=2020303030;
+	System.out.println(bigDouble);
+	
+	String bigDouble_String =Double.toString(bigDouble);
+	System.out.println(bigDouble_String);
 }	
 }
