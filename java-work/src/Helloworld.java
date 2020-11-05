51798ada4ae4c6a3b9a05437aebb7b1cf6498c3b
Prashanth Kanukuntla <Prashanth Kanukuntla@DESKTOP-2C66BHS>
2020-11-05 06:33:49 +0530

    Basic program

--- java-work/src/Helloworld.java
+++ java-work/src/Helloworld.java
@@ -1,8 +1,11 @@
 
 public class Helloworld {
 	
+	static String Read_value="print the static values";
+	static final double PInum=3.141529;
 	public static void main(String[] args) {
-		System.out.println("hello programming world");
+		System.out.println(Read_value+"\n");
+		System.out.println("the value of the PI is: "+PInum);
 	}
  
 	}
