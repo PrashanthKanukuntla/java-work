2470dd7c9bd6e9d15631bf9c05c2c3e53c39e828
Prashanth Kanukuntla <Prashanth Kanukuntla@DESKTOP-2C66BHS>
2020-11-05 23:16:15 +0530

    take input from user and create nested if- else loop condition.

--- java-work/src/Helloworld.java
+++ java-work/src/Helloworld.java
@@ -1,17 +1,24 @@
+import java.util.*;
 public class Helloworld{
 	public static void main(String[] args) {
-		int boy=25;
-		int girl=24;
+		//int boy=25;
+		//int girl=24;
+	Scanner cat= new Scanner(System.in);
+	System.out.println("Enter the boy age");
+		int boy=cat.nextInt();
+	System.out.println("Enter the girl age");
+		int girl=cat.nextInt();
 		
-		if(boy>21) {
-			System.out.println("The boy is eligible for marriage");
-			if(girl>18) {
-				System.out.println("the girl is eligible for marriage");
+		if(boy>=21) {
+			System.out.println("The boy is : "+boy+ " and eligible for marriage");
+			if(girl>=18) {
+				System.out.println("the girl is : "+girl+ " and eligible for marriage");
 			}else {
-				System.out.println("the girl is not eligible for marriage");
+				System.out.println("the girl is : "+girl+" and not eligible for marriage");
 			}
 		}else {
-				System.out.println("the boy is not eligible for marriage");
+				System.out.println("the boy is : "+boy+" and not eligible for marriage");
 		}
+		cat.close();
 	}
 }
