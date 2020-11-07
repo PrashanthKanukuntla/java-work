a2a6d7fd78b9b5c900b92dee88dc764fe1adc8a2
Prashanth Kanukuntla <Prashanth Kanukuntla@DESKTOP-2C66BHS>
2020-11-08 02:22:27 +0530

    Average of N number of elements

--- java-work/src/Helloworld.java
+++ java-work/src/Helloworld.java
@@ -2,25 +2,14 @@ public class Helloworld
 {
 public static void main(String[] args)
 {
-	int a[][]= {{1,2,3},{4,5,6},{7,8,9}};
-	int b[][]= {{2,3,5},{3},{6,7,8,9}};
-	
-	System.out.println("this is the first array");
-	display(a);
-	
-	System.out.println("this is the second array");
-	display(b);
+	System.out.println(average(33,44,67,98));
 }
 
-	public static void display(int x[][]) 
-	{
-	for(int row=0; row<x.length; row++)
-	{
-		for(int column=0; column<x[row].length;column++)
-		{
-			System.out.print(x[row][column]+"\t");
-		}
-			System.out.println();
-	}
-	}
+public static int average(int ... number)
+{
+	int total=0;
+	for(int x:number)
+		total+=x;
+	return total/number.length;
+}
 }
