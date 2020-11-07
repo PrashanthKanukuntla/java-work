a9430e1e36282666c4fb19c16c0a5666d293e44d
Prashanth Kanukuntla <Prashanth Kanukuntla@DESKTOP-2C66BHS>
2020-11-07 17:35:20 +0530

    compound interest and for loop

--- java-work/src/Helloworld.java
+++ java-work/src/Helloworld.java
@@ -1,20 +1,21 @@
-import java.util.Scanner;
-public class Helloworld{
-	public static void main(String[] args) {
-		Scanner sun= new Scanner(System.in);
-		
-		System.out.println("enter the grades");
-		
-		int count=0;
-		int grade;
-		int total=0;
-		int average;
-		while(count<10) {
-			grade =sun.nextInt();
-			total+=grade;
-			count++;
-		}
-		average= total/10;
-		System.out.println("the average is :-"+average);
+import java.util.*;
+public class Helloworld
+{
+public static void main(String[] args)
+{
+	Scanner shark= new Scanner(System.in);
+	
+	double amount;
+	
+	System.out.println("enter the price");
+	double price=shark.nextDouble();
+	
+	System.out.println("enter the rate");
+	double rate=shark.nextDouble();
+	
+	for(int day=1; day<=20; day++) {
+		amount=price*Math.pow(1+rate, day);
+		System.out.println(day+" "+amount);
 	}
+}
 }
