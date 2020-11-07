62c4bb09451c6ecb92d88547ff0c0fa1d625d78f
Prashanth Kanukuntla <Prashanth Kanukuntla@DESKTOP-2C66BHS>
2020-11-08 01:40:00 +0530

    Multidimentional array

--- java-work/src/Helloworld.java
+++ java-work/src/Helloworld.java
@@ -2,10 +2,25 @@ public class Helloworld
 {
 public static void main(String[] args)
 {
-	int a[]= {1,2,3,4,5};
-	for(int x:a)
-	{
-		System.out.println(x);
-	}
+	int a[][]= {{1,2,3},{4,5,6},{7,8,9}};
+	int b[][]= {{2,3,5},{3},{6,7,8,9}};
+	
+	System.out.println("this is the first array");
+	display(a);
+	
+	System.out.println("this is the second array");
+	display(b);
 }
+
+	public static void display(int x[][]) 
+	{
+	for(int row=0; row<x.length; row++)
+	{
+		for(int column=0; column<x[row].length;column++)
+		{
+			System.out.print(x[row][column]+"\t");
+		}
+			System.out.println();
+	}
+	}
 }
